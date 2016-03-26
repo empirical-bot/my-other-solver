49dd44f0127ee21c1d8230c9bbdbed3a1698f9a0
alantrrs <alan.trrs.ngls@gmail.com>
2016-03-25 20:05:10 -0700

    Initial commit

---
+++ solver.py
@@ -0,0 +1,8 @@
+import empirical
+
+class Solver(object):
+  def solve(self, x, y):
+    solution = 42 # the answer to life the universe and everything
+    return solution
+
+empirical.run(Solver())
